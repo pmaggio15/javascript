13097e406c028e3710e2dd69f4ec4f2b26519738
Paige <pmaggio15@gmail.com>
2025-06-11 21:59:06 -0600

    loops one through 20

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-//let count = 1;
-
-//while (count <= 100) {
-//console.log (count)
-//count = count + 1;
-//}
-
-for (let i = 1; i <= 20; ++i) {
-    if (i % 3 === 0 && i % 5 ===0) {
-        console.log(`${i} -> Frontend Simplified`)
+for (i= 1; i <= 20; ++i) {
+    if (i % 3 ===0 && i % 5 ===0) {
+        console.log (`${i} -> Frontend Simplified`)
     }
     else if (i % 3 === 0) {
-        console.log(`${i} -> Frontend`)
+        console.log (`${i} -> Frontend`)
+        
     }
     else if (i % 5 === 0) {
-        console.log(`${i} -> Simplified`)
+        console.log (`${i} -> Simplified`)
     }
     else {
         console.log (`${i} -> ${i}`)
