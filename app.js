775d032ddaef4217e927d00cc6e578421ff6ec80
Paige <pmaggio15@gmail.com>
2025-06-15 19:39:01 -0600

    DOM

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-let arr = [20, 30, 40, 50, 100]
-
-// First element of array ;
-console.log (arr[0])
-
-// Las element of array;
-console.log(arr[arr.length - 1])
-
-//Add element onto end of array
-arr.push (200)
-
-console.log (arr);
+function toggleDarkMode() {
+    document.querySelector('body').classList.toggle("dark-theme")
+}
