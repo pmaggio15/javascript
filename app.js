dc45502658357382b8bd4121fff22ed5cacfe140
Paige <pmaggio15@gmail.com>
2025-06-27 21:08:23 -0600

    array

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-function toggleDarkMode() {
-    document.querySelector('body').classList.toggle("dark-theme")
+function getMax(arr) {
+    let max = arr[0]
+    for (let i = 0 ; i < arr.length; ++i) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;  
 }
+
+console.log(getMax([-100,-200,-300]));
